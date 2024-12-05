faa167a40362fdce99ffccbf1d13620516376725
aldeevN <aldeev33@gmail.com>
2024-12-05 18:53:29 +0300

    add fortune wheel

--- js/script.js
+++ js/script.js
@@ -2,15 +2,18 @@ const playBtn = document.getElementById('playBtn')
 const listItems = document.getElementById('items')
 const listTasks = document.getElementById('tasks')
 const attempts = document.getElementById('fortune__title-h2')
-
+const fortuneRoulette = document.getElementById('fortune__roulette')
+const smallCircle = document.getElementById('small__circle')
+const root = document.querySelector(':root')
 let attemptCount = 2
 const items = [
-    { value: "калифорния-каппа-маки-в-кунжуте", win: false },
-    { value: "лава-с-крабом", win: false },
-    { value: "нежный-с-курицей", win: false },
-    { value: "филадельфия-лайт-сяке", win: false },
-    { value: "ЭТОФИЛА", win: false },
-    { value: "Apple-iPhone-12-Transparent", win: false },
+    { value: "Apple-iPhone-12-Transparent", src: "./img/Apple-iPhone-12-Transparent.png", win: false },
+    { value: "филадельфия-лайт-сяке", src: "./img/филадельфия-лайт-сяке.png", win: false },
+    { value: "калифорния-каппа-маки-в-кунжуте", src: "./img/калифорния-каппа-маки-в-кунжуте.png", win: false },
+    { value: "-20%", src: "./img/20.png", win: false },
+    { value: "нежный-с-курицей", src: "./img/нежный-с-курицей.png", win: false },
+    { value: "лава-с-крабом", src: "./img/лава-с-крабом.png", win: false },
+    { value: "ЭТОФИЛА ", src: "./img/ЭТОФИЛА.png", win: false },
 ]
 const tasks = [
     { value: "Подпишитесь на сообщество", attempt: "+1 попытка", completed: false },
@@ -57,18 +60,32 @@ tasks.forEach(task => {
 })
 items.forEach(item => {
     let listItem = document.createElement('li')
+    let img = document.createElement('img')
     listItem.className = "list__item"
+    img.src = item.src
+    img.className = "item__img"
     listItems.appendChild(listItem)
+    listItem.appendChild(img)
 
 })
+let timeout
 function play() {
-    const res = Math.floor(Math.random() * items.length)
-    items[res].win = true
-    attemptCount--
-    alert("you are been win!: " + items[res].value)
-    attempts.innerHTML = attemptCount.toString()
+    fortuneRoulette.classList.add("fortune__roulette-animation")
+    smallCircle.classList.add("small__circle-animatin")
+    timeout = setTimeout(alertWin, 3000)
 }
 
+function alertWin() {
+    const res = Math.floor(Math.random() * items.length)
+    const deg = `${Math.floor(res * 360 / 7 + 360)}deg`
+    root.style.setProperty('--deg', deg)
+    alert("you are been win!: " + items[res].value)
+    items[res].win = true
+    attemptCount--
+    attempts.innerHTML = attemptCount.toString()
+    fortuneRoulette.classList.remove("fortune__roulette-animation")
+    smallCircle.classList.remove("small__circle-animatin")
+}
 playBtn.addEventListener('click', () => {
     attemptCount ? play() : alert("try do some tasks")
 })
